d1385ac1fced5ccc2ed9956fe12e4202af61cc13
Pursuecoding <131742876+Pursuecoding@users.noreply.github.com>
2023-08-06 23:02:34 -0400

    Added SearchEngine scenario to the component

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,8 +1,48 @@
 import axios, {isCancel, AxiosError} from 'axios';
 function SearchEngine() {
-    return (
-        "Hello"
-    );
+    const [city, setCity] = useState("");
+  const [temperature, setTemperature] = useState(null);
+  const [description, setDescription] = useState(null);
+  const [humidity, setHumidity] = useState(null);
+  const [wind, setWind] = useState(null);
+  //const [icon, setIcon] = useState(null);
+
+  function displayWeather(response) {
+    setTemperature(Math.round(response.data.main.temp));
+    setDescription(response.data.weather[0].description);
+    setHumidity(response.data.main.humidity);
+    setWind(Math.round(response.data.wind.speed));
+    //setIcon(response.data.weather[0].icon);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=eb9542c65e739e0fb25ade97c749e2aa&units=metric`;
+    axios.get(url).then(displayWeather);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="search"
+          placeholder="Type a city"
+          autoFocus={true}
+          onChange={updateCity}
+        />
+        <input type="submit" value="Search" />
+      </form>
+      <ul>
+        <li>Temperature is {temperature}°C</li>
+        <li>Description: {description}</li>
+        <li>Humidity: {humidity}%</li>
+        <li>Wind: {wind} km/h</li>
+      </ul>
+    </div>
+  );
 }
 
 export default SearchEngine;
