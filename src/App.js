e0556e4f0da76261d06e6eacc2bde6a248cb7e0f
Pursuecoding <131742876+Pursuecoding@users.noreply.github.com>
2023-08-06 23:20:08 -0400

    Added link to GitHub

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ function App() {
     <div className="App">
       <h1>React Search Engine</h1>
       <SearchEngine />
+      <p>This app is 
+      <a href="https://github.com/Pursuecoding/my-app" _target="blank">Open-sourced</a>
+      </p>
     </div>
   );
 }
