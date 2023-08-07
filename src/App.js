8d175d2cd08e9a0d2eb650bb5cc0fa36e4dfa25d
Pursuecoding <131742876+Pursuecoding@users.noreply.github.com>
2023-08-06 23:52:10 -0400

    Fixed spelling errors

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <h1>React Search Engine</h1>
       <SearchEngine />
       <p>This app is 
-      <a href="https://github.com/Pursuecoding/my-app" _target="blank">Open-sourced</a>
+      <a href="https://github.com/Pursuecoding/my-app" _target="blank"> Open-sourced</a>
       </p>
     </div>
   );
